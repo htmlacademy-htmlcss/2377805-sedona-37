7dd362e86d14e3ac2c0cf7f736dd118acfa6bd49
WhyItWorksAlex <fishman1q2w3e@gmail.com>
2023-07-06 18:56:53 +0300

    Исправлены замечания по итогам проверки работы наставником ч1

--- scritps/script.js
+++ scritps/script.js
@@ -1,29 +1,29 @@
 if (document.querySelector('.arrival-date .date-example')) {
-    document.querySelector('.arrival-date').addEventListener('click', ()=> {
-    document.querySelector('.arrival-date .date-example').remove();
-    });
+  document.querySelector('.arrival-date').addEventListener('click', ()=> {
+  document.querySelector('.arrival-date .date-example').remove();
+  });
 }
 
 if (document.querySelector('.departure-date .date-example')) {
-    document.querySelector('.departure-date').addEventListener('click', ()=> {
-    document.querySelector('.departure-date .date-example').remove();
-    });
+  document.querySelector('.departure-date').addEventListener('click', ()=> {
+  document.querySelector('.departure-date .date-example').remove();
+  });
 }
 
 if (document.querySelector('.arrival-date .date-input + .date-example')) {
-    document.querySelector('.arrival-date .date-input').addEventListener('focus', ()=> {
-    document.querySelector('.arrival-date .date-input + .date-example').remove();
-    });
+  document.querySelector('.arrival-date .date-input').addEventListener('focus', ()=> {
+  document.querySelector('.arrival-date .date-input + .date-example').remove();
+  });
 }
 
 if (document.querySelector('.departure-date .date-input + .date-example')) {
-    document.querySelector('.departure-date .date-input').addEventListener('focus', ()=> {
-    document.querySelector('.departure-date .date-input + .date-example').remove();
-    });
+  document.querySelector('.departure-date .date-input').addEventListener('focus', ()=> {
+  document.querySelector('.departure-date .date-input + .date-example').remove();
+  });
 }
 
 document.querySelector('.modal-close-button').addEventListener('click', ()=> {
-    document.querySelector('.modal-container').classList.add('visually-hidden')});
+  document.querySelector('.modal-container').classList.add('visually-hidden')});
 
 document.querySelector('.button-booking').addEventListener('click', ()=> {
-    document.querySelector('.modal-container').classList.remove('visually-hidden')});
+  document.querySelector('.modal-container').classList.remove('visually-hidden')});
